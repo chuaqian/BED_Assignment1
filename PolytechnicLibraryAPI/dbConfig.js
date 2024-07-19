34301457c8f9f7e329391a8b418086ac17e6293f
rayannfoo <s10257860@connect.np.edu.sg>
2024-07-19 20:34:01 +0800

    week 11 and 12 practical

--- PolytechnicLibraryAPI/dbConfig.js
+++ PolytechnicLibraryAPI/dbConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   user: "bed_assignment", // Replace with your SQL Server login username
-  password: "IlgFwi4*p0Ts!", // Replace with your SQL Server login password
+  password: "bed", // Replace with your SQL Server login password
   server: "localhost",
   database: "bed_db",
   trustServerCertificate: true,
